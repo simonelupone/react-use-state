222fc453f84aa3278892345aa473760d24db9de4
simonelupone <simone.lupone@gmail.com>
2025-05-08 18:29:16 +0200

    feat: add dynamic info when button is clicked

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,14 @@ import { useState } from "react"
 const Main = ({ languages }) => {
 
     const [active, setActive] = useState(null)
+    const activeItem = languages.find((lang) => lang.id === active)
 
     return (
         <main>
-            <div className="w-full mt-20 flex justify-center">
+            <div className="w-full mt-20 flex flex-col items-center justify-center">
                 <div className="w-7xl">
 
-                    <div className="">
+                    <div>
                         {languages.map((lang) => {
                             return (
                                 <button onClick={() => setActive(lang.id)} key={`lang-${lang.id}`} className={`font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer ${active === lang.id ? 'bg-yellow-500 text-black' : 'bg-blue-700 text-white'}`}>{lang.title}</button>
@@ -18,6 +19,16 @@ const Main = ({ languages }) => {
                     </div>
 
                 </div>
+
+                <div className="w-7xl mt-10 p-4 border border-gray-300 rounded">
+                    {activeItem && (
+                        <div className="">
+                            <h3 className="font-bold text-xl pb-2">{activeItem.title}</h3>
+                            <p>{activeItem.description}</p>
+                        </div>
+                    )}
+                </div>
+
             </div>
         </main >
     )
